bc8137c536c89c7029c4e9500c3939fd93bfebee
Andy Blight <a.j.blight@leeds.ac.uk>
2022-08-16 12:11:40 +0100

    Added screen shots of security and password entry dialogs.
    Updated eduroam.md to match.

--- eduroam.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Installs packages needed to connect to Eduroam.
-
-sudo apt update
-sudo apt install network-manager network-manager-gnome openvpn openvpn-systemd-resolved network-manager-openvpn network-manager-openvpn-gnome
-sudo apt purge openresolv dhcpcd5
-sudo ln -sf /lib/systemd/resolv.conf /etc/resolv.conf
