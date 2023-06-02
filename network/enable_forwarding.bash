2c68ae1deafe1b18f3696b09592ff5d83c61a69c
Andy Blight <a.j.blight@leeds.ac.uk>
2023-06-02 13:35:29 +0100

    DNS stil fails.

--- network/enable_forwarding.bash
+++ network/enable_forwarding.bash
@@ -6,10 +6,10 @@ INTERNET_IF="wlp0s20f3"
 ROBOT_NETWORK_IF="enp0s31f6"
 
 echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
-sudo iptables -t nat -A POSTROUTING -o ${INTERNET_IF} -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o ${ROBOT_NETWORK_IF} -j MASQUERADE
 sudo iptables -A FORWARD -i ${INTERNET_IF} -o ${ROBOT_NETWORK_IF} -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i ${ROBOT_NETWORK_IF} -o ${INTERNET_IF} -j ACCEPT
-sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+# sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 sudo systemctl restart systemd-networkd
 
 sudo iptables -L
