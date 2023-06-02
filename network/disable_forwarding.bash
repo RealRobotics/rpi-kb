48fb7e3251ae1e7a3ec40f945a8a5280d37504d9
Andy Blight <a.j.blight@leeds.ac.uk>
2023-06-02 13:04:25 +0100

    Can ping 8.8.8.8 from Pi.  DNS is dead on PC and Pi

--- network/disable_forwarding.bash
+++ network/disable_forwarding.bash
@@ -6,8 +6,10 @@ INTERNET_IF="wlp0s20f3"
 ROBOT_NETWORK_IF="enp0s31f6"
 
 echo 0 | sudo tee /proc/sys/net/ipv4/ip_forward
-sudo iptables -D FORWARD -i ${ROBOT_NETWORK_IF} -o ${INTERNET_IF} -j ACCEPT
-sudo iptables -D FORWARD -i ${INTERNET_IF} -o ${ROBOT_NETWORK_IF} -m state --state ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -t nat -D POSTROUTING -o ${INTERNET_IF} -j MASQUERADE
+sudo iptables -D FORWARD -i ${INTERNET_IF} -o ${ROBOT_NETWORK_IF} -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -D FORWARD -i ${ROBOT_NETWORK_IF} -o ${INTERNET_IF} -j ACCEPT
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+sudo service networking restart
 
 sudo iptables -L -t nat
